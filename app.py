cb3d892112b684a8a37c7757af74d210ecfcc6e6
aa-tolmachev <taa.tolmachev@gmail.com>
2018-11-22 00:41:22 +0500

    ADD - app add threaded

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def score_v1():
 if __name__ == "__main__":
     #heroku
     port = int(os.getenv('PORT', 5000))
-    application.run(debug=False, port=port, host='0.0.0.0')
+    application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
     #local
     #application.run()
